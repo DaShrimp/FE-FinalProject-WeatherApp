043992f564be367addc000bbfd1a7a445fed48c4
DaShrimp <nhatminhvo2311@gmail.com>
2024-10-14 15:51:14 +0700

    add comment

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// KHAI BAO CAC BIEN DE SU DUNG
 const
     userLocation = document.getElementById("userLocation"),
     converter = document.getElementById("converter"),
@@ -20,11 +21,16 @@ const
 const
     Forecast = document.querySelector(".Forecast");
 
-
+// GOI API
 const
+    // API LAY TTHONG TIN CHUNG
     WEATHER_API_ENDPOINT = `https://api.openweathermap.org/data/2.5/weather?appid=a5bb4718b30b6f58f58697997567fffa&q=`,
+    // API LAY THONG TIN CHI TIET
     WEATHER_DATA_ENDPOINT = `https://api.openweathermap.org/data/2.5/onecall?appid=a5bb4718b30b6f58f58697997567fffa&exclude=minutely&units=metric&`,
+    // API LAY THONG TIN ZIP/POST CODE
     WEATHER_ZIP_ENDPOINT = `https://api.openweathermap.org/data/2.5/weather?appid=a5bb4718b30b6f58f58697997567fffa&zip=`;
+
+// KHAI BAO KIEU DU LIEU THOI GIAN
 const
     options1 = {
         hour: "numeric",
@@ -40,11 +46,13 @@ const
         hour12: true,
     };
 
+// HAM TIM VI TRI NGUOI DUNG VA TRA VE THONG TIN THHOI TIET
 function findLocation() {
     Forecast.innerHTML = "";
+    // ID ELSE DE XU LY TRUONG HOP NGUOI DUNG NHAP VAO ZIP CODE POST CODE HOAC NHAP VAO TEN THANH PHO
     if (typeof userLocation.value === 'number') {
+        // GOI API XU LY ZIP CODE
         fetch(WEATHER_ZIP_ENDPOINT + userLocation.value).then((res) => res.json()).then(data => {
-
             if (data.cod != "" && data.cod != 200) {
                 alert(data.message);
                 return
@@ -54,6 +62,7 @@ function findLocation() {
             getWeatherData(data);
         });
     } else {
+        // GOI API XU LY TEN THANH PHO
         fetch(WEATHER_API_ENDPOINT + userLocation.value).then((res) => res.json()).then(data => {
             if (data.cod != "" && data.cod != 200) {
                 alert(data.message);
@@ -64,21 +73,22 @@ function findLocation() {
             getWeatherData(data);
         });
     }
-
 }
 
+// HAM XU LY THOONG TIN VA TRA VE KET QUA
 function getWeatherData(data) {
-
+    // THAY DOI TEN TP VA ICON NHIET DO
     city.innerHTML = data.name + ", " + data.sys.country;
     weatherIcon.style.background = `url(https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png)`;
-
+    // LAY LATIYUTE VA LONGTITUTE TU DU LIEU CUA API ZIP HOAC API CHUNG
     fetch(`${WEATHER_DATA_ENDPOINT}&lon=${data.coord.lon}&lat=${data.coord.lat}`).then((res) => res.json()).then(data => {
+        // LAY DATA TU API VA IN RA KHUNG WEATHER CHINH
         console.log(data);
         temperature.innerHTML = TempConvert(data.current.temp);
         feelsLike.innerHTML = "Feels like " + data.current.feels_like;
         description.innerHTML = `<i class = "fa-brands fa-cloudversify"></i> &nbsp;` + data.current.weather[0].description;
         date.innerHTML = getLongFormatDateTime(data.current.dt, data.timezone_offset, options);
-
+        // LAY DATA TU API VA IN RA THONG TIN LIEN QUAN
         HValue.innerHTML = Math.round(data.current.humidity) + "<span>%</span>";
         WValue.innerHTML = Math.round(data.current.wind_speed) + "<span>m/s</span>";
         SRValue.innerHTML = getLongFormatDateTime(data.current.sunrise, data.timezone_offset, options1);
@@ -86,7 +96,7 @@ function getWeatherData(data) {
         CValue.innerHTML = data.current.clouds + "<span>%</span>";
         UVValue.innerHTML = data.current.uvi;
         PValue.innerHTML = data.current.pressure + "<span>hPa</span>";
-
+        // LAY DATA TU API VA TAO CAC O DATA CAC NGAY TRONG TUAN
         data.daily.forEach((weather) => {
             let div = document.createElement("div");
             let daily = getLongFormatDateTime(weather.dt, 0, options).split(" at ");
@@ -98,13 +108,21 @@ function getWeatherData(data) {
         });
     });
 }
+// TU DONG RELOAD THONG TIN SAU KHI THAY DOI LOAI NHIET DO
+converter.addEventListener("change", () => {
+    if (userLocation.value) {
+        findLocation();
+    }
+});
 
+// XU LY THONG TIN THOI GIAN
 function getLongFormatDateTime(dtValue, offSet, options) {
     const unixTime = dtValue + offSet;
     const date = new Date(unixTime * 1000);
     return date.toLocaleString("en-US", { ...options, timeZone: 'UTC' });
 }
 
+// XU LY THONG TIN KHI DOI TU DO C -> F
 function TempConvert(temp) {
     let tempValue = Math.round(temp);
     let message = "";
@@ -116,9 +134,3 @@ function TempConvert(temp) {
     }
     return message;
 }
-
-converter.addEventListener("change", () => {
-    if (userLocation.value) {
-        findLocation();
-    }
-});
